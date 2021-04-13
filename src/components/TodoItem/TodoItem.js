6626ad97449c1ca5c8550bba4a05b8cc8720db1a
Ersin <ckmrsn@gmail.com>
2021-04-13 17:06:42 +0300

    Initialized

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -1,9 +1,18 @@
-import React from "react";
-import "./TodoItem.css";
+import React from 'react'
+import './TodoItem.css'
 
 function Todo(props) {
-  const { content } = props;
-  return <div>{content}</div>;
+	const { id, content, removeTodo } = props
+
+	const toggleComplete = (e) => {
+		e.target.firstElementChild.classList.toggle('complete')
+	}
+	return (
+		<div className='todoItem' onClick={toggleComplete}>
+			<div>{content}</div>
+			<button onClick={() => removeTodo(id)}>Sil</button>
+		</div>
+	)
 }
 
-export default Todo;
+export default Todo
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,21 +1,25 @@
-import React from "react";
-import TodoItem from "../TodoItem/TodoItem";
-import "./TodoList.css";
+import React from 'react'
+import TodoItem from '../TodoItem/TodoItem'
+import './TodoList.css'
 
 function TodoList(props) {
-  return (
-    <div>
-      <ul>
-        {props.todos.map((todo) => {
-          return (
-            <li className="list-group-item">
-              <TodoItem {...todo} key={todo.id} />
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<ul>
+				{props.todos.map((todo) => {
+					return (
+						<li className='list-group-item'>
+							<TodoItem
+								{...todo}
+								key={todo.id}
+								removeTodo={props.removeTodo}
+							/>
+						</li>
+					)
+				})}
+			</ul>
+		</div>
+	)
 }
 
-export default TodoList;
+export default TodoList
